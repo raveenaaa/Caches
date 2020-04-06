4eeab469610b0e13e0f9a00c191e15c382e9bae1
rmdcosta@ncsu.edu <rmdcosta@ncsu.edu>
2020-04-06 01:44:18 -0400

    task 3: completed

--- meow.io/routes/index.js
+++ meow.io/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-// REDIS
 const redis = require('redis');
 let client = redis.createClient(6379, '127.0.0.1', {});
 
@@ -11,16 +10,18 @@ const cache = 'cache';
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  // await client.get(cache, function(err, cacheValue) {
-  //   if (cacheValue != null) {
-  //     res.render('index', { title: 'meow.io', recentUploads: await db.recentCats(5), bestFacts: JSON.parse(cacheValue) });
-  //     next();
-  //   }
-  // })
-  const bestFacts = (await db.votes()).slice(0,100);
-  // await client.set(cache, JSON.stringify(bestFacts));
-  // await client.expire(cache, 10);
-  res.render('index', { title: 'meow.io', recentUploads: await db.recentCats(5), bestFacts: bestFacts});
+    const uploads = await db.recentCats(5);
+    await client.get(cache, async function(err, cacheValue) {
+    if (cacheValue != null) {
+      res.render('index', { title: 'meow.io', recentUploads: uploads, bestFacts: JSON.parse(cacheValue) });
+    }
+    else {
+      const bestFacts = (await db.votes()).slice(0,100);
+      await client.set(cache, JSON.stringify(bestFacts));
+      await client.expire(cache, 10);
+      res.render('index', { title: 'meow.io', recentUploads: uploads, bestFacts: bestFacts});
+    }
+    });
 });
 
 module.exports = router;
